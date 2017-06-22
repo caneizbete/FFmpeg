799281dd4fc7d8d09c4789d4f15b9a93899d981f
caneizbete <caneizbete@users.noreply.github.com>
2017-06-22 15:54:23 +0200

    minihack

--- libavformat/rtsp.c
+++ libavformat/rtsp.c
@@ -1691,7 +1691,7 @@ redirect:
     av_url_split(proto, sizeof(proto), auth, sizeof(auth),
                  host, sizeof(host), &port, path, sizeof(path), s->filename);
 
-    if (!strcmp(proto, "rtsps")) {
+    if (!strcmp(proto, "rtsps") || !strcmp(proto, "rtsp")) {
         lower_rtsp_proto         = "tls";
         default_port             = RTSPS_DEFAULT_PORT;
         rt->lower_transport_mask = 1 << RTSP_LOWER_TRANSPORT_TCP;
